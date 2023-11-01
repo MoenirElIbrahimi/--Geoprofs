6b7c29fe37bd947402edf784d8087244d63bdd8e
Juul van Tilburg <56611802+Juulvt@users.noreply.github.com>
2023-11-01 16:23:04 +0100

    get balance for days

--- Pages/leavebalance.cshtml.cs
+++ Pages/leavebalance.cshtml.cs
@@ -24,62 +24,47 @@ namespace ContosoUniversity.Pages
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-
-            if (_context.Employees == null)
-            {
-                return NotFound();
-            }
-
             Employee employee;
 
             if (id == null)
             {
-                var userId = HttpContext.Session.GetInt32("UserId");
+                var userId = HttpContext.Session.GetInt32("UserID");
                 if (userId == null)
                 {
                     return NotFound();
                 }
 
-                var user = await _context.Users.FirstOrDefaultAsync(m => m.ID == userId);
-                if (user == null)
+                var user = await _context.Users
+                    .Include(u => u.Employee) // Include the Employee data
+                    .FirstOrDefaultAsync(m => m.ID == userId);
+
+                if (user == null || user.Employee == null)
                 {
                     return NotFound();
                 }
 
                 employee = user.Employee;
+            }
+            else
+            {
+                employee = await _context.Employees
+                    .FirstOrDefaultAsync(m => m.ID == id);
 
                 if (employee == null)
                 {
                     return NotFound();
                 }
-                else
-                {
-                    Employee = employee;
-                    // Fetch the leave requests associated with the employee
-                    Leaverequests = await _context.Leaverequests
-                                        .Include(lr => lr.Status) // Include the Status entity
-                                        .Where(lr => lr.Employee.ID == employee.ID && lr.Status.Name == "Accepted") // Filter by the 'Accepted' status
-                                        .ToListAsync();
-                }
-                return Page();
-
             }
 
-            employee = await _context.Employees.FirstOrDefaultAsync(m => m.ID == id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                Employee = employee;
-                // Fetch the leave requests associated with the employee
-                Leaverequests = await _context.Leaverequests
-                                        .Include(lr => lr.Status) // Include the Status entity
-                                        .Where(lr => lr.Employee.ID == employee.ID && lr.Status.Name == "Accepted") // Filter by the 'Accepted' status
-                                        .ToListAsync();
-            }
+            Employee = employee;
+            // Fetch the leave requests associated with the employee
+            Leaverequests = await _context.Leaverequests
+                .Include(lr => lr.Status) // Include the Status entity
+                .Where(lr => lr.Employee.ID == employee.ID && lr.Status.Name == "Accepted") // Filter by the 'Accepted' status
+                .ToListAsync();
+
             return Page();
         }
+
     }
 }
