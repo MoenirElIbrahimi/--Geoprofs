18184a4794b7bf39e096e32f656fa3834666c139
Juul van Tilburg <56611802+Juulvt@users.noreply.github.com>
2023-12-07 16:15:53 +0100

    Startdate and Enddate only show date

--- Models/Leaverequest.cs
+++ Models/Leaverequest.cs
@@ -1,4 +1,6 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public class Leaverequest
     {
@@ -9,8 +11,10 @@
 
         public Status Status { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
 
         public Employee Employee { get; set; }
